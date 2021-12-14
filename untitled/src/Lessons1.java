ccac3b7b098bc1c2776f3fb10c0eb4580db29c95
fibber2240 <96056157+fibber2240@users.noreply.github.com>
2021-12-14 12:28:52 +0300

    Массивы
    1.Инициализация массивов
    2.Вывод массивов
    3.Вывод только четных массивов
    4.Умножение всех элементов массивов
    5.Вывод массивов в обратном порядке

---
+++ untitled/src/Lessons1.java
@@ -0,0 +1,31 @@
+public class Lessons1 {
+    public static void main(String[] args) {
+        // обявляю и инициализирую массив
+        int[] numbers = {3,4,5,6,7,8};
+
+        // вывожу все элементы массива
+        for (int i = 0; i<numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
+        
+        //вывожу только четные элементы массива
+        for (int i = 0; i<numbers.length; i++){
+            if (i%2==1){
+                System.out.println(numbers[i]);
+            }
+        }
+
+        // умножаю все элементы массива на 0,5 и вывожу результат
+        for (int i = 0; i<numbers.length; i++){
+            System.out.println(numbers[i]*0.5);
+        }
+
+        // вывожу элементы массива в обратном порядке
+        for (int i=numbers.length-1; i>=0; i--){
+            System.out.println(numbers[i]);
+        }
+    }
+}
+
+
+
