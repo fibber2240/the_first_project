59ac9e9d948c4267b47c646ad747c688754f26f9
fibber2240 <96056157+fibber2240@users.noreply.github.com>
2022-01-01 23:56:28 +0300

    add empty

--- untitled/src/Lessons2.java
+++ untitled/src/Lessons2.java
@@ -3,8 +3,8 @@ public class Lessons2 {
         public static void main(String[] args) {
             int [][] numbers = new int[10][10];
             // объявляем массив размером 10*10
-            for (int a =0; a<10; a++ ){
-                for (int b=0; b<10;b++){
+            for (int a =0; a<numbers.length; a++ ){
+                for (int b=0; b<numbers[a].length;b++){
                     System.out.print(numbers [a][b] +" ");
                 }
                 System.out.println();
