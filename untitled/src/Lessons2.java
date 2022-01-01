3d45694080066d72362c39baa0c3f33da4826c67
fibber2240 <96056157+fibber2240@users.noreply.github.com>
2022-01-01 11:08:28 +0300

    Двумерный массив

---
+++ untitled/src/Lessons2.java
@@ -0,0 +1,14 @@
+
+import java.util.Arrays;
+public class Lessons2 {
+        public static void main(String[] args) {
+            int [][] numbers = new int[10][10];
+            // объявляем массив размером 10*10
+            for (int a =0; a<10; a++ ){
+                for (int b=0; b<10;b++){
+                    System.out.print(numbers [a][b] +" ");
+                }
+                System.out.println();
+            }
+        }
+    }
