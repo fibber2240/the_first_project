bdc29450b4538510e2002ebd04764aacee7e2049
fibber2240 <96056157+fibber2240@users.noreply.github.com>
2022-01-02 22:45:33 +0300

    Тренировка с 2х мерном массивом.Запонение и поиск по индексу

--- untitled/src/Lessons2.java
+++ untitled/src/Lessons2.java
@@ -1,17 +1,37 @@
+import com.sun.org.apache.xpath.internal.objects.XObject;
+
 import java.util.Arrays;
 public class Lessons2 {
         public static void main(String[] args) {
-            int [][] numbers = new int[10][10];
-            // объявляем массив размером 10*10
-            for (int a =0; a<numbers.length; a++ ){
-                for (int b=0; b<numbers[a].length;b++){
-                    System.out.print(numbers [a][b] +" ");
+            Object
+            int[][] cube = new int [10][10];
+            int i = 0;
+            int j = 0;
+            int a = 0;
+            int b = 9;
+            // Заполнение массива и поиск
+            for(;i < cube.length; i++) {
+                for(; j < cube[i].length; j++) {
+                    cube[i][j] = a;
+                    cube[i + 1][j] = b;
+                    a++;
+                    b--;
+                    if(cube[i][j] == 3 | cube[i][j] == 8) {
+                        System.out.print(cube[i][j] + " ");
+                    }
+                    if(cube[i + 1][j] == 6 | cube[i + 1][j] == 4) {
+                        System.out.print(cube[i + 1][j] + " ");
+                    }
                 }
-                System.out.println();
             }
-            // проверка
-            System.out.println(Arrays.asList(numbers[3][8]).indexOf(0));
-            System.out.println(Arrays.asList(numbers[4][6]).indexOf(0));
-            System.out.println(Class.forName());
+            // Можно посмотреть вывод всей матрицы
+        System.out.println();
+         for (int[] anArr : cube) {
+             for (int anAnArr : anArr) {
+                 System.out.print(anAnArr + " ");
+             }
+             System.out.println();
+         }
         }
 }
+
